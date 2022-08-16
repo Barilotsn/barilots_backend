1cbb5259e716ec76cd8b718488c3afb8e1b8549f
Narendra <backend_engineer@barilots.com>
2022-08-16 19:28:57 +0530

    add server folder

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import cors from 'cors';
-import connectDB from './config/connectdb.js'
-import userRoutes from './routes/userRoutes.js'
+import connectDB from '../config/connectdb.js'
+import userRoutes from '../routes/userRoutes.js'
 
 const app = express()
 const port = process.env.PORT
